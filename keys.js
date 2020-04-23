836f4ff9c8bc67de3feabddba5e52fdac5fec15c
Yevhen Oliynyk <yevhen@crowdin.com>
2020-04-23 12:35:36 +0300

    Move keys to host env

--- keys.js
+++ keys.js
@@ -5,22 +5,14 @@ module.exports = {
   baseUrl: isDev
     ? "http://172.21.21.60:8000" // dev
     : "https://crowdin-typeform.herokuapp.com/",
-  crowdinClientId: isDev
-    ? "trjBbjAue27tY7Hw0phH"  // dev
-    : "79aTqdRo8oAFFZMLl773",
-  crowdinClientSecret: isDev
-    ? "n3fsQOtG3D2fqPhBwkBq02A7HnsmUzaxHIWy9FpN" // dev
-    : "OaHvAdFOKdxV5Y0yMBayE9zAX09Gxy1cYCUBkXTD",
-  integrationClientId: isDev
-    ? "FzzC2UBbCjhNt9CYACYXvfo42P572xXhDkVoKKBkddtM" // dev
-    : "FH6eFmebzgprreXJDFp1JS7oczabvGHnqgdgqQV1Pe7L",
-  integrationSecret: isDev
-    ? "Ea3GVEYKJtJkQHzHXVJ5QueR2eeEaDXF1sbjG7TLZhbz" // dev
-    : "HPXNKSixdtK5G2TJXZb6qfH4PsThd7dAYfw6a4C27JWD",
+  crowdinClientId: process.env.CROWDIN_CLIENT_ID || "trjBbjAue27tY7Hw0phH", // dev
+  crowdinClientSecret: process.env.CROWDIN_CLIENT_SECRET || "n3fsQOtG3D2fqPhBwkBq02A7HnsmUzaxHIWy9FpN", // dev
+  integrationClientId: process.env.INTEGRATION_CLIENT_ID || "FzzC2UBbCjhNt9CYACYXvfo42P572xXhDkVoKKBkddtM", // dev
+  integrationSecret: process.env.INTEGRATION_CLIENT_SECRET || "Ea3GVEYKJtJkQHzHXVJ5QueR2eeEaDXF1sbjG7TLZhbz", // dev
   callbackUrl: isDev
     ? "http://172.21.21.60:8000/integration-token" // dev
     : "https://crowdin-typeform.herokuapp.com/integration-token",
-  cryptoSecret: '78a10623f59sdhfghjgfjghac1b8e02ebde4b58f94',
+  cryptoSecret: process.env.CRYPTO_SECRET || '78a10623f59sdhfghjgfjghac1b8e02ebde4b58f94',
   crowdinAuthUrl : isDev && useCopyAuth
     ? "http://accounts.yevhen.dev.crowdin.com/oauth/token" // Local copy auth service
     : "https://accounts.crowdin.com/oauth/token"
