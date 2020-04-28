99e1a025ca2913b5f97edbb42d741ae0bc9280b1
Yevhen Oliynyk <yevhen@crowdin.com>
2020-04-28 09:45:39 +0300

    fix url

--- keys.js
+++ keys.js
@@ -4,12 +4,12 @@ const useCopyAuth = false;
 module.exports = {
   baseUrl: isDev
     ? "http://172.21.21.60:8001" // dev
-    : "https://crowdin-typeform.herokuapp.com/",
+    : "https://sendgrid-crowdin.herokuapp.com/",
   crowdinClientId: process.env.CROWDIN_CLIENT_ID || "trjBbjAue27tY7Hw0phH", // dev
   crowdinClientSecret: process.env.CROWDIN_CLIENT_SECRET || "n3fsQOtG3D2fqPhBwkBq02A7HnsmUzaxHIWy9FpN", // dev
   callbackUrl: isDev
     ? "http://172.21.21.60:8001/integration-token" // dev
-    : "https://crowdin-typeform.herokuapp.com/integration-token",
+    : "https://sendgrid-crowdin.herokuapp.com/integration-token",
   cryptoSecret: process.env.CRYPTO_SECRET || '78a10623f59sdhfghjgfjghac1b8e02ebde4b58f94',
   crowdinAuthUrl : isDev && useCopyAuth
     ? "http://accounts.yevhen.dev.crowdin.com/oauth/token" // Local copy auth service
